09e610cd315d4030173eb6a9e8a52165d68ef991
Agroball <rotimQW@gmail.com>
2021-08-31 22:07:33 +0300

    33

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 app.use(cors({
-  origin: 'https://hakuna.matata.nomoredomains.club',
+  origin: 'https://agroball.sharli.nomoredomains.work',
   credentials: true,
 }));
 
